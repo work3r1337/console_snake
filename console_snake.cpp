289d781df1b77ea5c63b5e7cd36d92600564a87f
work3r1337 <c.michasov2004@yandex.ru>
2025-09-27 19:15:21 +0300

    Fix food bug

--- console_snake.cpp
+++ console_snake.cpp
@@ -32,7 +32,7 @@ class Game {
   public:
 	Game(int h = DEFAULT_HEIGHT, int w = DEFAULT_WIDTH) : height(h), width(w){};
 
-	void start_game() {
+	void start_game() const {
 		if (!initscr()) {
 			throw std::runtime_error("Failed to initialize ncurses");
 		}
@@ -47,11 +47,11 @@ class Game {
 		}
 	}
 
-	void end_game() { endwin(); }
+	void end_game() const { endwin(); }
 
-	int get_height() { return height; }
+	int get_height() const { return height; }
 
-	int get_width() { return width; }
+	int get_width() const { return width; }
 };
 
 class Snake {
@@ -89,9 +89,9 @@ class Snake {
 
 	void grow() { snake.push_back(snake.back()); }
 
-	void change_direction(Direction d) { direction = d; }
+	void change_direction(const Direction &d) { direction = d; }
 
-	bool is_snake_part(Point p) {
+	bool is_snake_part(const Point &p) const {
 		for (auto iter = snake.begin(); iter != snake.end(); iter++) {
 			if (*iter == p)
 				return true;
@@ -99,7 +99,7 @@ class Snake {
 		return false;
 	}
 
-	bool eat_itself() {
+	bool eat_itself() const {
 		for (auto iter1 = snake.begin(); iter1 != snake.end(); iter1++) {
 			for (auto iter2 = std::next(iter1); iter2 != snake.end(); iter2++) {
 				if ((*iter1) == (*iter2))
@@ -109,7 +109,7 @@ class Snake {
 		return false;
 	}
 
-	size_t size() { return snake.size(); }
+	size_t size() const { return snake.size(); }
 };
 
 class Food {
@@ -118,18 +118,20 @@ class Food {
 	Point p;
 
   public:
-	void respawn() {
+	void respawn(const Snake &snake) {
 		std::random_device rd;
 		std::mt19937 gen(rd());
 		std::uniform_int_distribution<> distX(0, game.get_width() - 1);
 		std::uniform_int_distribution<> distY(0, game.get_height() - 1);
-		p.x = distX(gen);
-		p.y = distY(gen);
+		do {
+			p.x = distX(gen);
+			p.y = distY(gen);
+		} while (snake.is_snake_part(p));
 	}
 
 	Food(Game &g) : game(g){};
 
-	Point get_position() { return p; };
+	Point get_position() const { return p; };
 };
 
 class Console_Renderer {
@@ -141,7 +143,7 @@ class Console_Renderer {
   public:
 	Console_Renderer(Game &g, Snake &s, Food &f) : game(g), snake(s), food(f){};
 
-	void render_game() {
+	void render_game() const {
 		for (int y = 0; y < game.get_height(); y++) {
 			for (int x = 0; x < game.get_width(); x++) {
 				if (snake.is_snake_part(Point(x, y))) {
@@ -157,7 +159,7 @@ class Console_Renderer {
 		refresh();
 	}
 
-	void render_game_over() {
+	void render_game_over() const {
 		clear();
 		mvprintw(game.get_height() / 2, game.get_width() / 2 - 5, "GAME OVER");
 		mvprintw(game.get_height() / 2 + 1, game.get_width() / 2 - 5,
@@ -165,7 +167,7 @@ class Console_Renderer {
 		refresh();
 	}
 
-	void render_score() {
+	void render_score() const {
 		mvprintw(game.get_height(), game.get_width() / 2 - 5, "Score: %zu",
 				 snake.size() - 1);
 		refresh();
@@ -181,7 +183,7 @@ int main() {
 	Snake snake(game);
 	Food food(game);
 	game.start_game();
-	food.respawn();
+	food.respawn(snake);
 	Console_Renderer renderer(game, snake, food);
 	while (!snake.eat_itself()) {
 		int ch = getch();
@@ -200,7 +202,7 @@ int main() {
 			break;
 		}
 		if (check_collision(food, snake)) {
-			food.respawn();
+			food.respawn(snake);
 			snake.grow();
 		}
 		snake.move();
